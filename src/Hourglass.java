f28627e6bf18f330c5a04e079f9591077fc7a850
sanchale000 <sanchale000@CAM-LAB-18.bgsd.k12.wa.us>
2019-10-08 07:52:14 -0700

    i dont want to add comments

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -3,5 +3,17 @@
 */
 
 public class Hourglass {
-
+	public static final int LINES = 4;
+	public static void bases() {
+		System.out.print("|");
+		int c = LINES * 2 + 2;
+		for (int i = 1; i <= c; i++) {
+			System.out.print("\"");
+		}
+		System.out.print("|");
+	}
+	public static void main(String[] args) {
+		bases();
+		
+	}
 }
